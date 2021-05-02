2b97e7671f3f88da3bf1a5a0e258fc8acb3dd8df
rustshubkinn <rustshubkinn@mail.ru>
2021-05-02 13:58:25 +0600

    Added Javascript for Burger Button in Header Module.

--- src/blocks/components/burger-menu/burger-menu.js
+++ src/blocks/components/burger-menu/burger-menu.js
@@ -1,3 +1,5 @@
 const burgerButton = document.querySelector(".burger");
 
 burgerButton.addEventListener("click", () => burgerButton.classList.toggle("change"));
+
+export default burgerButton;
--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,1 +1,10 @@
 import "../../../js/import/components.js";
+import burgerButton from "../../components/burger-menu/burger-menu.js";
+
+// UI Elements
+const mobileMenu = document.querySelector(".mobile-menu");
+
+// Event Listeners
+burgerButton.addEventListener("click", function openMenu() {
+  mobileMenu.classList.toggle("burger-clicked");
+});
