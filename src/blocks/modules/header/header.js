a08ec17e06388136b3c0be41b0dd9df407a86477
rustshubkinn <rustshubkinn@mail.ru>
2021-05-04 17:42:00 +0600

    Changed the way of getting burgerButton from Import to querySelector

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,8 +1,6 @@
-import "../../../js/import/components.js";
-import burgerButton from "../../components/burger-menu/burger-menu.js";
-
 // UI Elements
 const mobileMenu = document.querySelector(".mobile-menu");
+const burgerButton = document.querySelector(".burger");
 
 // Event Listeners
 burgerButton.addEventListener("click", function openMenu() {
