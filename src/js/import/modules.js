c9b985a3ff9bb008a4f8d851b8964b57c1d430e6
rustshubkinn <rustshubkinn@mail.ru>
2021-05-04 17:39:12 +0600

    Fixed Javascript Imports

---
+++ src/js/import/modules.js
@@ -0,0 +1,2 @@
+import "../../blocks/modules/header/header.js";
+import "../../blocks/modules/feedback/feedback.js";
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,2 @@
 import "./import/modules";
 import "./import/components";
-import "../blocks/modules/header/header.js";
